19cade9612a1dfe2996bf9e1502bdb3996fbf35b
Shaiv Patel <shaivpatel410@gmail.com>
2019-01-10 00:18:13 -0500

    emailMatcher now can parseFiles with no errors but needs to be tested

--- emailMatcher.py
+++ emailMatcher.py
@@ -1,19 +1,104 @@
+from openpyxl import load_workbook
+from models.persons import Client, Employee
+from models.emails import EmailRow, Email
+import pandas
+######################## Load Files ###############################
+email_records_file_path =  "EmailRecordsPseudoData.xlsx"
+sales_force_contacts_path =  "SalesForcePseudoData.csv"
 
+salesforce_contacts_df = pandas.read_csv(sales_force_contacts_path)
+email_records_ws = load_workbook(filename=email_records_file_path).worksheets[0]
 
+###################################################################
 
+######################## Assign Columns ###########################
+date_col = "A"
+id_col = "D"
+email_address_col = "C"
+type_col = "F"
+subject_col = "E"
+domain_col = "B"
 
-
-
-
-
-
+company_name = "CompanyName"
+company_domain = "Domain"
+###################################################################
 
 employees = {}
 clients = {}
 
-def parseEmailRecords(emails_xlsx):
-    #Open xlsx in read only mode
-    #Iterate row by row
-        #check if row Type is 'FROM"
 
-    pass
+
+
+
+def parseSalesForce(df):
+    clients = {}
+    for row_index, row in df.iterrows():
+        companyName = row[company_name]
+        companyDomain = row[company_domain]
+        clients[companyDomain] = Client(companyName, companyDomain)
+    return clients
+
+
+def parseEmailRecords(emails):
+    row_cursor = 2
+    email_buffer = []
+    is_sender_employee = False
+    while row_cursor<emails.max_row:
+        e_row = EmailRow([cell.value for cell in list(emails[f"A{row_cursor}:F{row_cursor}"][0])])
+
+
+        if e_row.type  == 'FROM':
+
+            if len(email_buffer)>1:
+                Email(email_buffer,employees,clients)
+            email_buffer = []
+
+            if e_row.domain != "macquarie.com" and e_row.domain not in clients:
+                row_id = e_row.id
+                while emails[f"{id_col}{row_cursor}"].value == row_id:
+                    row_cursor += 1
+            else:
+                if e_row.domain == "macquarie.com":
+                    is_sender_employee = True
+                email_buffer.append(e_row)
+                row_cursor += 1
+        else:
+            if is_sender_employee:
+                if e_row.domain in clients:
+                    email_buffer.append(e_row)
+            else:
+                if emails[f"{domain_col}{row_cursor}"].value == "macquarie.com":
+                    email_buffer.append(e_row)
+            row_cursor += 1
+
+
+
+
+if __name__ == "__main__":
+
+    clients = parseSalesForce(salesforce_contacts_df)
+
+    parseEmailRecords(email_records_ws)
+
+
+    print("===========================\nEmployee\n=============================")
+
+    for employee in employees.values():
+        print(f"===========================\n{employee}\n=============================")
+        print("=============\nSent\n==============")
+        for s in employee.sentEmails:
+            print(s)
+        print("=============\nReceived\n==============")
+        for r in employee.receivedEmails:
+            print(r)
+
+    print("===========================\nClients\n=============================")
+
+    for client in clients.values():
+        print(f"===========================\n{client}\n=============================")
+        print("=============\nSent\n==============")
+        for s in client.sentEmails:
+            print(s)
+        print("=============\nReceived\n==============")
+        for r in client.receivedEmails:
+            print(r)
--- models/emails.py
+++ models/emails.py
@@ -13,16 +13,21 @@ class Email():
 
         if sender_row.domain == "macquarie.com":
             if sender_row.email_address not in employees:
-                employee_email = sender_row.email_address
-                employee_name = employee_email.split("@")[0].replace("."," ").title()
-                employees[sender_row.email_address] = Employee(employee_name,employee_email)
+                employees[sender_row.email_address] = Employee(sender_row.email_address)
 
-            self.setSender(employees[email_rows.email_address])
+            self.setSender(employees[sender_row.email_address])
 
             for email_row in email_rows[1:]:
                 self.addReceiver(clients[email_row.domain])
+        else:
 
 
+            self.setSender(clients[sender_row.domain])
+
+            for email_row in email_rows[1:]:
+                if email_row.email_address not in employees:
+                    employees[email_row.email_address] = Employee(email_row.email_address)
+                self.addReceiver(employees[email_row.email_address])
 
     def addReceiver(self,receiver):
         self.receivers.append(receiver)
@@ -48,11 +53,11 @@ class Email():
 class EmailRow():
 
     date_index = 0
-    id_index = 1
+    id_index = 3
     email_address_index = 2
-    type_index = 3
+    type_index = 5
     subject_index = 4
-    domain = 5
+    domain_index = 1
 
     def __init__(self,row_array):
         self.date = row_array[EmailRow.date_index]
@@ -61,6 +66,9 @@ class EmailRow():
         self.type = row_array[EmailRow.type_index]
         self.subject = row_array[EmailRow.subject_index]
         self.domain = row_array[EmailRow.domain_index]
+
+    def __str__(self):
+        return f"date:{self.date} | id:{self.id} | email_address:{self.email_address} | type:{self.type} | subject:{self.subject} | domain:{self.domain}"
 
 class EmailThread():
 
